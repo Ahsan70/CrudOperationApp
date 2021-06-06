bdfc8ca00369fe21289ddd82e1675353df7e5a6e
Ahsan Ali Siddiqi <44543152+Ahsan70@users.noreply.github.com>
2021-06-06 21:45:11 +0500

    Added Customer and MembershipType Domain Class

--- CrudOperationUsingEF/Models/IdentityModels.cs
+++ CrudOperationUsingEF/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace CrudOperationUsingEF.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
