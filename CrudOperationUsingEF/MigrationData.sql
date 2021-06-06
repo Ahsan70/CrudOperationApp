905e1caaa3fecab0dae6ffc3b8d1ac6b9a543c5f
Ahsan Ali Siddiqi <44543152+Ahsan70@users.noreply.github.com>
2021-06-06 21:46:38 +0500

    Added MigrationData SQL File for Data Seeding Purpose

---
+++ CrudOperationUsingEF/MigrationData.sql
@@ -0,0 +1,4 @@
+﻿ Insert into MembershipTypes (Id,SignUpFee,DurationInMonths,DiscountRate) values(1,0,0,0);
+  Insert into MembershipTypes (Id,SignUpFee,DurationInMonths,DiscountRate) values(2,30,1,10);
+ Insert into MembershipTypes (Id,SignUpFee,DurationInMonths,DiscountRate) values(3,90,3,15);
+
