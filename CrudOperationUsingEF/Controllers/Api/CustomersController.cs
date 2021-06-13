4b1f6c2f1a9ad53599acff666299045fa063b721
Ahsan Ali <ahsan-ali70@outlook.com>
2021-06-13 12:29:08 -0700

    Building An API

---
+++ CrudOperationUsingEF/Controllers/Api/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using CrudOperationUsingEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrudOperationUsingEF.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public CustomersController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _dbContext.Customers.ToList();
+        }
+        public Customer GetCustomer(int id)
+        {
+            var customer = _dbContext.Customers.SingleOrDefault(c=> c.Id==id);
+            if (customer != null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return customer; 
+        }
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            _dbContext.Customers.Add(customer);
+            _dbContext.SaveChanges();
+            return customer;
+        }
+        [HttpPut]
+        public void UpdateCustomer(int id,Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _dbContext.Customers.SingleOrDefault(x => x.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            customerInDb.Name = customer.Name;
+            customerInDb.BirthDate = customer.BirthDate;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            _dbContext.SaveChanges();
+
+        }
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _dbContext.Customers.SingleOrDefault(x => x.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _dbContext.Customers.Add(customerInDb);
+            _dbContext.SaveChanges();
+        }
+    }
+}
